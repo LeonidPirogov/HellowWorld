3f5bf244a3866af73bdc0b363d84a47aa845f9a5
LeonidPirogov <lv.pirogof@gmail.com>
2025-04-11 14:04:10 +0300

    Initial Commit

---
+++ HellowWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HellowWorld
+//
+//  Created by Leonid on 11.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
